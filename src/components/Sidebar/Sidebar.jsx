4a0d128398ddcb2669e2bded53307619c458224b
Edison <aalevenegass@gmail.com>
2024-03-03 15:51:50 -0300

    Agregando links de nvegación react-router-dom

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import "../Sidebar/sidebar.css";
 
 const Sidebar = () => {
@@ -6,7 +7,27 @@ const Sidebar = () => {
       <div className="sidebar__first__section">
         <div className="sidebar__logo__section">
           <i class="bx bxl-spotify sidebar__icon"></i>
-          <p className="sidebar__logo__paragraph">Spotify</p>
+          <p className="sidebar__logo__paragraph sidebar__logo__paragraph__spotify">
+            Spotify
+          </p>
+        </div>
+        <div className="sidebar__logo__inicio">
+          <NavLink className="sidebar__logo__link">
+            <span className="material-symbols-outlined sidebar__icon sidebar__icon__inicio">
+              home
+            </span>
+            <p className="sidebar__logo__paragraph sidebar__logo__paragraph__inicio__section">
+              Inicio
+            </p>
+          </NavLink>
+        </div>
+        <div className="sidebar__logo__inicio">
+          <NavLink className="sidebar__logo__link">
+            <i className="bx bx-search sidebar__icon sidebar__icon__inicio"></i>
+            <p className="sidebar__logo__paragraph sidebar__logo__paragraph__inicio__section">
+              Buscar
+            </p>
+          </NavLink>
         </div>
       </div>
     </section>
