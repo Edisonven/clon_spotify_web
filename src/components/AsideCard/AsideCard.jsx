da6b2d877375ccd3a2837d0a06e7255033874f11
Edison <aalevenegass@gmail.com>
2024-03-04 15:06:42 -0300

    creando componente card para sidebar

---
+++ src/components/AsideCard/AsideCard.jsx
@@ -0,0 +1,11 @@
+import "../AsideCard/asidecard.css";
+
+const AsideCard = () => {
+  return (
+    <section className="asidecard__container">
+      <h1>Hola soy la card</h1>
+    </section>
+  );
+};
+
+export default AsideCard;
