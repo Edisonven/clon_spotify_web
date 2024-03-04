c807a9f3f4e191bc4c7e153947d59b811c4d87c9
Edison <aalevenegass@gmail.com>
2024-03-04 20:56:45 -0300

    Creando componente ventana modal

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="footer__title__section">
         <h4 className="footer__title">Muestra de Spotify</h4>
         <h5 className="footer__descrip">
-          Regístrate para acceder a canciones y podcast ilimitados con algunos
+          Regístrate para acceder a canciones y podcasts ilimitados con algunos
           anuncios. No necesitas tarjeta de crédito.
         </h5>
       </div>
---
+++ src/components/PlaylistModal/PlaylistModal.jsx
@@ -0,0 +1,11 @@
+import "../PlaylistModal/playlistmodal.css";
+
+const PlaylistModal = () => {
+  return (
+    <section className="modal__container">
+      <h1>Hola soy nuna modal</h1>
+    </section>
+  );
+};
+
+export default PlaylistModal;
