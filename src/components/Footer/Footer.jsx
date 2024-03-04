4e13cf9d30671525f665d83aef955637555d3bde
Edison <aalevenegass@gmail.com>
2024-03-04 18:29:53 -0300

    Ajustando contenido y estilos de footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,16 @@ import "../Footer/footer.css";
 const Footer = () => {
   return (
     <section className="footer__container">
-      <div>Hola soy el footer</div>
+      <div className="footer__title__section">
+        <h4 className="footer__title">Muestra de Spotify</h4>
+        <h5 className="footer__descrip">
+          Regístrate para acceder a canciones y podcast ilimitados con algunos
+          anuncios. No necesitas tarjeta de crédito.
+        </h5>
+      </div>
+      <div className="footer__register__section">
+        <button className="footer__register__btn">Regístrate gratis</button>
+      </div>
     </section>
   );
 };
