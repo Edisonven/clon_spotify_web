dc824e7dfb797f52ff5a89c97959cfc0d27edfdd
Edison <aalevenegass@gmail.com>
2024-03-05 00:19:41 -0300

    Añadiendo botones a navbar y estilando fondo de sección principal

--- src/components/Principal/Principal.jsx
+++ src/components/Principal/Principal.jsx
@@ -1,9 +1,15 @@
 import "../Principal/principal.css/";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const Principal = () => {
   return (
     <section className="principal__container">
-      
+      <nav className="principal__navbar">
+        <div className="principal__navbar__icons">
+          <IoIosArrowBack className="principal__navbar__icon" />
+          <IoIosArrowForward className="principal__navbar__icon" />
+        </div>
+      </nav>
     </section>
   );
 };
