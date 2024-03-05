04f868950972fc3397bfbf530a599a506f592369
Edison <aalevenegass@gmail.com>
2024-03-04 23:52:21 -0300

    Ajustando sección principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer/Footer.jsx";
+import Principal from "./components/Principal/Principal.jsx";
 import Sidebar from "./components/Sidebar/Sidebar.jsx";
 
 function App() {
   return (
     <section className="app__container">
       <Sidebar />
+      <Principal />
       <Footer />
     </section>
   );
---
+++ src/components/Principal/Principal.jsx
@@ -0,0 +1,11 @@
+import "../Principal/principal.css/";
+
+const Principal = () => {
+  return (
+    <section className="principal__container">
+      
+    </section>
+  );
+};
+
+export default Principal;
