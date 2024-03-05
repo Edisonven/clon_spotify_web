f5f7c01624269538d2a07e19d1713d1cc18ec1a0
Edison <aalevenegass@gmail.com>
2024-03-05 11:48:54 -0300

    Creando componente card para sección principal

---
+++ src/components/PrincipalCard/PrincipalCard.jsx
@@ -0,0 +1,11 @@
+import "../PrincipalCard/principalcard.css";
+
+const PrincipalCard = () => {
+  return (
+    <div>
+      <h1>Hoola soy la card</h1>
+    </div>
+  );
+};
+
+export default PrincipalCard;
