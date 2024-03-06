b2f4a784a968126530c508dcc00756b8982a1986
Edison <aalevenegass@gmail.com>
2024-03-06 13:08:27 -0300

    Asignando estilos a la vista inicio de sesión

--- src/components/views/SingUp/SingUp.jsx
+++ src/components/views/SingUp/SingUp.jsx
@@ -1,9 +1,17 @@
 import "../SingUp/singup.css";
+import SpotifyHome from "../../SpotifyHome/SpotifyHome";
 
 const SingUp = () => {
   return (
     <section className="singup__container">
-      <h1>Hola soy el inicio de sesión</h1>
+      <nav className="singup__navbar">
+        <SpotifyHome to="/" className="singup__logo__link__spotify">
+          <i className="bx bxl-spotify singup__icon"></i>
+          <p className="singup__paragraph singup__logo__paragraph singup__logo__paragraph__spotify">
+            Spotify
+          </p>
+        </SpotifyHome>
+      </nav>
     </section>
   );
 };
