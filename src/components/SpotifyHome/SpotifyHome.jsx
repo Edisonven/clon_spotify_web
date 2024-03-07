82336e013485670d873753909dbb6faf840fda9e
Edison <aalevenegass@gmail.com>
2024-03-07 00:09:14 -0300

    AAplicando estilos por defecto en componente botón de spotify

--- src/components/SpotifyHome/SpotifyHome.jsx
+++ src/components/SpotifyHome/SpotifyHome.jsx
@@ -1,8 +1,14 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
 
-const SpotifyHome = ({ to, className, children }) => {
+const SpotifyHome = ({ to, className, children, style }) => {
+  const defaultStyle = {
+    width: "max-content",
+  };
+  const mergedStyle = { ...defaultStyle, ...style };
+
   return (
-    <NavLink to={to} className={className}>
+    <NavLink to={to} className={className} style={mergedStyle}>
       <i className={className}></i>
       <p className={className}></p>
       {children}
