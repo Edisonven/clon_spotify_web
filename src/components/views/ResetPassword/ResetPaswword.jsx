be6704a8078448d272b355e8bfc83aaf28bc67d9
Edison <aalevenegass@gmail.com>
2024-03-07 13:13:25 -0300

    Añadiendo estilos a vista reestablecer contraseña

--- src/components/views/ResetPassword/ResetPaswword.jsx
+++ src/components/views/ResetPassword/ResetPaswword.jsx
@@ -1,9 +1,11 @@
 import "../ResetPassword/resetpassword.css";
 import SpotifyHome from "../../SpotifyHome/SpotifyHome";
+import { NavLink } from "react-router-dom";
+import Button from "../../Button/Button";
 
 const ResetPassword = () => {
   return (
-    <section className="ressetpassword__container">
+    <section className="resetpassword__container">
       <nav className="singup__navbar">
         <SpotifyHome to="/" className="singup__logo__link__spotify">
           <i className="bx bxl-spotify singup__icon"></i>
@@ -12,6 +14,36 @@ const ResetPassword = () => {
           </p>
         </SpotifyHome>
       </nav>
+      <section className="resetpassword__body__section">
+        <h1 className="resetpassword__body__title">Restablece tu contraseña</h1>
+        <p className="resetpassword__body__section__desc">
+          Ingresa tu dirección de email o nombre de usuario y te enviaremos un
+          enlace para volver a ingresar a tu cuenta.
+        </p>
+        <span className="resetpassword__body__paragraph">
+          Dirección de email o nombre de usuario
+        </span>
+        <form className="resetpassword__body__form">
+          <input className="resetpassword__form__input" type="text" />
+          <NavLink className="resetpassword__form__help">
+            ¿Necesitas ayuda?
+          </NavLink>
+          <Button className="resetpassword__form__btn">Enviar enlace</Button>
+        </form>
+      </section>
+      <footer className="resetpassword__footer">
+        <p className="resetpassword__footer__paragraph">
+          This site is protected by reCAPTCHA and the Google{" "}
+          <NavLink className="resetpassword__footer__link">
+            Privacy Policy
+          </NavLink>{" "}
+          and{" "}
+          <NavLink className="resetpassword__footer__link">
+            Terms of service
+          </NavLink>{" "}
+          apply.
+        </p>
+      </footer>
     </section>
   );
 };
