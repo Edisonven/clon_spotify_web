bd431c9972b81ba9c6e4ca941cb95ee0ac00d2e9
Edison <aalevenegass@gmail.com>
2024-03-08 11:42:03 -0300

    Cambiando ruta del archivo json

--- src/components/contexts/ArtistContext.jsx
+++ src/components/contexts/ArtistContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 
 export const ArtistContext = createContext();
 
-const artistJs = "/src/components/ArtistCard/artists.json";
+const artistJs = "/artists.json";
 
 const ArtistProvider = ({ children }) => {
   const [artistData, setArtistData] = useState([]);
