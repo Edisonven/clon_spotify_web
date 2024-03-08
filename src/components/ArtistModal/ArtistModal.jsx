b01683a043043dbc6e459f2769351bf5a5604558
Edison <aalevenegass@gmail.com>
2024-03-08 16:40:06 -0300

    Agregando botones y descripción en modal

--- src/components/ArtistModal/ArtistModal.jsx
+++ src/components/ArtistModal/ArtistModal.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import "../ArtistModal/artistmodal.css";
 import { ArtistContext } from "../contexts/ArtistContext";
+import Button from "../Button/Button";
+import { NavLink } from "react-router-dom";
 
 const ArtistModal = () => {
   const { artistModal } = useContext(ArtistContext);
@@ -27,6 +29,18 @@ const ArtistModal = () => {
           <h1 className="artistmodal__desc__title">
             Empieza a escuchar con una cuenta gratis de Spotify
           </h1>
+          <Button className="artistmodal__desc__btn">Registrarse gratis</Button>
+          <Button className="artistmodal__desc__btn artistmodal__desc__btn_download">
+            Registrarse gratis
+          </Button>
+          <div className="artistmodal__desc__link__section">
+            <p className="artistmodal__desc__link__paragraph">
+              ¿Ya tienes una cuenta?
+            </p>
+            <NavLink className="artistmodal__desc__link">
+              Iniciar sesión
+            </NavLink>
+          </div>
         </div>
       </div>
     </section>
