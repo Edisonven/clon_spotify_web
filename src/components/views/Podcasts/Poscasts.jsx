2e705bed7a359ea905c9889e78d66fe96c03e6ed
Edison <aalevenegass@gmail.com>
2024-03-14 18:33:33 -0300

    Mapeando podcasts y asignando estilos

--- src/components/views/Podcasts/Poscasts.jsx
+++ src/components/views/Podcasts/Poscasts.jsx
@@ -1,15 +1,31 @@
+import { useContext } from "react";
 import PrincipalNavbar from "../../PrincipalNavbar/PrincipalNavbar";
 import "../Podcasts/podcasts.css";
+import { PodcastsContext } from "../../contexts/PodcastsContext";
 
 const Podcasts = () => {
+  const { podcastsData } = useContext(PodcastsContext);
+
   return (
     <section className="principal__podcasts">
       <PrincipalNavbar className="principal__podcasts__nav" />
-      <div className="principal__podcasts__title__container">
-        <h1 className="principal__podcasts__title">Podcasts</h1>
-      </div>
-      <div className="principal__podcasts__container">
-
+      <div className="principal__podcasts__section">
+        <div className="principal__podcasts__title__container">
+          <h1 className="principal__podcasts__title">Podcasts</h1>
+        </div>
+        <div className="principal__podcasts__container">
+          {podcastsData?.map((podcast) => {
+            return (
+              <div
+                key={podcast.id}
+                style={{ background: podcast.color }}
+                className="principal__podcast__body"
+              >
+                <h1 className="principal__podcast__title">{podcast.name}</h1>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
