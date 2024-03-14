a59c73ece2abec9746ef1b086c35bb2df4f114b7
Edison <aalevenegass@gmail.com>
2024-03-14 18:48:25 -0300

    Creando título de categorías y estilando fondo de contenedor de podcasts

--- src/components/views/Podcasts/Poscasts.jsx
+++ src/components/views/Podcasts/Poscasts.jsx
@@ -14,17 +14,20 @@ const Podcasts = () => {
           <h1 className="principal__podcasts__title">Podcasts</h1>
         </div>
         <div className="principal__podcasts__container">
-          {podcastsData?.map((podcast) => {
-            return (
-              <div
-                key={podcast.id}
-                style={{ background: podcast.color }}
-                className="principal__podcast__body"
-              >
-                <h1 className="principal__podcast__title">{podcast.name}</h1>
-              </div>
-            );
-          })}
+          <h1 className="principal__podcasts__section__title">Categorías</h1>
+          <div className="principal__podcasts__body__section">
+            {podcastsData?.map((podcast) => {
+              return (
+                <div
+                  key={podcast.id}
+                  style={{ background: podcast.color }}
+                  className="principal__podcast__body"
+                >
+                  <h1 className="principal__podcast__title">{podcast.name}</h1>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </section>
