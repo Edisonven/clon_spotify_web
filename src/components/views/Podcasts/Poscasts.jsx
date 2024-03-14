38fdd620901718119c6dd21e3560a7c808f4c7dd
Edison <aalevenegass@gmail.com>
2024-03-14 17:56:50 -0300

    Creando json para mapear podcasts

--- src/components/views/Podcasts/Poscasts.jsx
+++ src/components/views/Podcasts/Poscasts.jsx
@@ -4,7 +4,13 @@ import "../Podcasts/podcasts.css";
 const Podcasts = () => {
   return (
     <section className="principal__podcasts">
-      <PrincipalNavbar className="principal__podcasts__nav"/>
+      <PrincipalNavbar className="principal__podcasts__nav" />
+      <div className="principal__podcasts__title__container">
+        <h1 className="principal__podcasts__title">Podcasts</h1>
+      </div>
+      <div className="principal__podcasts__container">
+
+      </div>
     </section>
   );
 };
