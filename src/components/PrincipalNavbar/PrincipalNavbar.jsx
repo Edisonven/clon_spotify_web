54f54a6431be1743d8f283037d25cec029ec2212
Edison <aalevenegass@gmail.com>
2024-03-15 22:11:33 -0300

    Ajustando estilos para propiedad text-overflow

--- src/components/PrincipalNavbar/PrincipalNavbar.jsx
+++ src/components/PrincipalNavbar/PrincipalNavbar.jsx
@@ -9,10 +9,12 @@ const PrincipalNavbar = ({ className, children }) => {
   };
   return (
     <nav className={`${className} principal__navbar`}>
-      <div className="principal__navbar__icons">
-        <IoIosArrowBack className="principal__navbar__icon" />
-        <IoIosArrowForward className="principal__navbar__icon" />
-        {children}
+      <div className="principal__navbar__icontitle__container">
+        <div className="principal__navbar__icons">
+          <IoIosArrowBack className="principal__navbar__icon" />
+          <IoIosArrowForward className="principal__navbar__icon" />
+        </div>
+        <div className="principal__navbar__title">{children}</div>
       </div>
       <div className="principal__register__section">
         <div className="principal__navbar__links">
