6783c66c2fefd509fecdb0c9e7ddad8cbd1c9f07
Edison <aalevenegass@gmail.com>
2024-03-27 11:21:37 -0300

    Estilando placeholder de barra de búsqueda

--- src/components/views/Search/Search.jsx
+++ src/components/views/Search/Search.jsx
@@ -1,11 +1,21 @@
 import PrincipalNavbar from "../../PrincipalNavbar/PrincipalNavbar";
 import "../Search/search.css";
+import { IoSearchOutline } from "react-icons/io5";
 
 const Search = () => {
   return (
     <section className="search__container">
       <PrincipalNavbar className="search__navbar">
-
+        <div className="search__navbar__input__container">
+          <IoSearchOutline className="search__navbar__icon" />
+          <input
+            className="search__navbar__input"
+            type="text"
+            name=""
+            id=""
+            placeholder="¿Qué quieres reproducir?"
+          />
+        </div>
       </PrincipalNavbar>
     </section>
   );
