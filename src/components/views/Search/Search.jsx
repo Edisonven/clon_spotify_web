36585cd2e08d27c43325cdf075a73f04c42c7fd7
Edison <aalevenegass@gmail.com>
2024-03-27 12:02:26 -0300

    Usando useRef para hacer focus al input por defecto

--- src/components/views/Search/Search.jsx
+++ src/components/views/Search/Search.jsx
@@ -1,14 +1,22 @@
+import { useRef, useEffect } from "react";
 import PrincipalNavbar from "../../PrincipalNavbar/PrincipalNavbar";
 import "../Search/search.css";
 import { IoSearchOutline } from "react-icons/io5";
 
 const Search = () => {
+  const searchInputRef = useRef();
+
+  useEffect(() => {
+    searchInputRef.current.focus();
+  }, []);
+
   return (
     <section className="search__container">
       <PrincipalNavbar className="search__navbar">
         <div className="search__navbar__input__container">
           <IoSearchOutline className="search__navbar__icon" />
           <input
+            ref={searchInputRef}
             className="search__navbar__input"
             type="text"
             name=""
